c10de8e8a7fd370198ca6bdf624b0ac0001f2930
Suvendu Patra <spatra@me.com>
2020-02-09 11:45:55 -0600

    not working

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,6 +10,5 @@ export class ProductService {
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => response.json());
-    return this._http.get(this._albumUrl).map(response => response.json());
   }
 }
